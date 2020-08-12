60af5f55efaef4ca2eaf44301d71ee54aec1de54
EC2 Default User <ec2-user@ip-172-31-18-248.ap-southeast-1.compute.internal>
2020-08-12 14:00:04 +0000

    初回ログイン時はニックネーム登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include ErrorUtils
   before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource)
+    if current_user.nickname.present?
+      root_path
+    else
+      user_path(current_user.id) # 主に初回ログイン時ニックネーム登録ページへ遷移
+    end
+  end
+
 end
