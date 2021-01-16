65ae2fc9ded7725b7cc2dd6cd656f025cc94ce84
EC2 Default User <ec2-user@ip-172-31-18-248.ap-southeast-1.compute.internal>
2021-01-16 03:56:14 +0000

    SendGridテスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    domain: 'teppan-neta.com',
+    domain: ENV['TEPPAN_DOMAIN'],
     port: 587,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
