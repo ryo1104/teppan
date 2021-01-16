88561a484463e9f592784f03236973697d6a45fb
EC2 Default User <ec2-user@ip-172-31-18-248.ap-southeast-1.compute.internal>
2021-01-16 09:47:16 +0000

    SendGridテスト

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
     address: 'smtp.sendgrid.net',
     domain: ENV['TEPPAN_DOMAIN'],
     port: 587,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    user_name: 'apikey',
+    password: ENV['SENDGRID_API_KEY'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
